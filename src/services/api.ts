1229dc6ac5a8cb662b2ae0a4354ccfa3e758e930
emekadefirst <victorchibuogwu33@gmail.com>
2026-01-29 17:15:36 +0100

    updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = 'https://api.deeplugg.com/v1';
-const API_URL = 'http://localhost:8000/v1';
+const API_URL = 'https://api.deeplugg.com/v1';
+//const API_URL = 'http://localhost:8000/v1';
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
